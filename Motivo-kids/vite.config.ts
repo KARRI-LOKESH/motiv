d5819a26649edf2952fb2abbdb1f22a87bfc14e5
KARRI-LOKESH <karrilokesh108@gmail.com>
2025-09-04 11:54:31 +0530

    header

--- Motivo-kids/vite.config.ts
+++ Motivo-kids/vite.config.ts
@@ -1,9 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist"
-  }
-});
+  base: "./"   // ✅ important for Vercel/Netlify
+})
